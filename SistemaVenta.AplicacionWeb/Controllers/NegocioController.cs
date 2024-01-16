3c82114b2dd05b677eb70efd5cdc6fcf9a094d83
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-01-15 22:29:15 -0600

    feat: Add `Negocio` interface and Implementation Class.

--- SistemaVenta.AplicacionWeb/Controllers/NegocioController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/NegocioController.cs
@@ -1,12 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Newtonsoft.Json;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
 
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class NegocioController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly INegocioService _negocioService;
+
+        public NegocioController(IMapper mapper, INegocioService negocioService)
+        {
+            _mapper = mapper;
+            _negocioService = negocioService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Obtener()
+        {
+            GenericResponse<VMNegocio> genericResponse = new GenericResponse<VMNegocio>();
+
+            try
+            {
+                VMNegocio negocio = _mapper.Map<VMNegocio>(await _negocioService.Obtener());
+
+                genericResponse.Estado = true;
+                genericResponse.Objeto = negocio;
+            }
+            catch (Exception ex)
+            {
+                genericResponse.Estado = false;
+                genericResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, genericResponse);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GuardarCambios([FromForm] IFormFile logo, [FromForm] string modelo)
+        {
+            GenericResponse<VMNegocio> genericResponse = new GenericResponse<VMNegocio>();
+
+            try
+            {
+                VMNegocio negocio = JsonConvert.DeserializeObject<VMNegocio>(modelo);
+
+                string nombreLogo = "";
+                Stream logoStream = null;
+
+                if (logo != null)
+                {
+                    string nombreEnCodigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(logo.FileName);
+                    nombreLogo = string.Concat(nombreEnCodigo, extension);
+                    logoStream = logo.OpenReadStream();
+                }
+
+                Negocio negocioEditado = await _negocioService.GuardarCambios(_mapper.Map<Negocio>(negocio), logoStream, nombreLogo);
+
+                negocio = _mapper.Map<VMNegocio>(negocioEditado);
+                
+                genericResponse.Estado = true;
+                genericResponse.Objeto = negocio;
+            }
+            catch (Exception ex)
+            {
+                genericResponse.Estado = false;
+                genericResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, genericResponse);
         }
     }
 }
---
+++ SistemaVenta.BLL/Implementacion/NegocioService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.Entity;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.BLL.Interfaces;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+    public class NegocioService : INegocioService
+    {
+        private readonly IGenericRepository<Negocio> _repositorio;
+        private readonly IFireBaseServices _fireBaseServices;
+
+        public NegocioService(IGenericRepository<Negocio> repositorio, IFireBaseServices fireBaseServices)
+        {
+            _repositorio = repositorio;
+            _fireBaseServices = fireBaseServices;
+        }
+
+        public async Task<Negocio> GuardarCambios(Negocio entidad, Stream Logo = null, string NombreLogo = "")
+        {
+            try
+            {
+                Negocio negocioEncontrado = await _repositorio.Obtener(n => n.IdNegocio == 1);
+                negocioEncontrado.NumeroDocumento = entidad.NumeroDocumento;
+                negocioEncontrado.Nombre = entidad.Nombre;
+                negocioEncontrado.Correo = entidad.Correo;
+                negocioEncontrado.Direccion = entidad.Direccion;
+                negocioEncontrado.Telefono = entidad.Telefono;
+                negocioEncontrado.PorcentajeImpuesto = entidad.PorcentajeImpuesto;
+                negocioEncontrado.SimboloMoneda = entidad.SimboloMoneda;
+                negocioEncontrado.NombreLogo = negocioEncontrado.NombreLogo == "" ? NombreLogo : negocioEncontrado.NombreLogo;
+
+                if (Logo != null)
+                {
+                    string urlFireBase = await _fireBaseServices.SubirStorage(Logo, "carpeta_logo", negocioEncontrado.NombreLogo);
+                    negocioEncontrado.UrlLogo = urlFireBase;
+                }
+
+                await _repositorio.Editar(negocioEncontrado);
+
+                return negocioEncontrado;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<Negocio> Obtener()
+        {
+            try
+            {
+                Negocio negocioEncontrado = await  _repositorio.Obtener(n => n.IdNegocio == 1);
+                return negocioEncontrado;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
